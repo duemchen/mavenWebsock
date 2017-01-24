c2d0992f30f9f6075d256a68ae7564a366861c2f
Wolfgang Dümchen <wolfgang.duemchen@simago.eu>
2017-01-24 15:02:47 +0100

    24.1. test websock und storage lokal

--- src/main/java/com/mycompany/mavenwebsock/heimWSEndpoint.java
+++ src/main/java/com/mycompany/mavenwebsock/heimWSEndpoint.java
@@ -5,6 +5,10 @@
  */
 package com.mycompany.mavenwebsock;
 
+import java.io.IOException;
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
@@ -16,23 +20,43 @@ import javax.websocket.server.ServerEndpoint;
  * @author duemchen
  */
 @ServerEndpoint("/endpoint")
+@Stateless
 public class heimWSEndpoint {
 
+    @Resource
+    ManagedExecutorService mes;
+
     @OnOpen
-    public void onOpen(Session sess){
-        System.out.println("open sess "+sess);
+    public void onOpen(Session sess) {
+        System.out.println("open sess " + sess);
+        final Session s = sess;
+        mes.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < 50; i++) {
+                        Thread.sleep(100);
+                        s.getBasicRemote().sendText(i + ". Message from server");
+                        System.out.println("i=" + i);
+                    }
+                    s.getBasicRemote().sendText("byebye.");
+                } catch (InterruptedException | IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 
     @OnClose
-    public void onClose(Session sess){
-        System.out.println("close sess "+sess);
+    public void onClose(Session sess) {
+        System.out.println("close sess " + sess);
     }
 
-    
     @OnMessage
     public String onMessage(String message, Session session) {
-        System.out.println("onMessage "+message);
+        System.out.println("onMessage " + message);
         return null;
     }
-    
+
 }
